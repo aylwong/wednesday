ff758994e43482f79127851192887086b683ff32
Silvio Silva <silvio.amaral@gmail.com>
2013-05-30 09:15:24 -0400

    Contains code to test the reading of file and parsing of strings

--- src/wednesday/core.clj
+++ src/wednesday/core.clj
@@ -1,9 +1,19 @@
 (ns wednesday.core
-	(:use clojure.string))
+  (:use clojure.string)
+  (:use clojure.java.io))
 
 (defn parseCommas [x] (split x #"\,"))
+
+(defn parseLines [x] (split x #"\r\n"))
+
+(defn parseCsvFromString [theString] 
+  (map parseCommas (parseLines theString)))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn readFile [filename]  (slurp filename))
+
+(defn parseFile [filename] (parseCsvFromString (readFile filename)))
--- test/wednesday/core_test.clj
+++ test/wednesday/core_test.clj
@@ -6,3 +6,29 @@
 (deftest parseCommas-Test
   (testing "The string did not parse into a list of 3 strings"
     (is (= ["This", "is", "test"] (parseCommas "This,is,test")))))
+
+(comment Test if we are properly parsing new lines)
+(deftest parseLines-Test
+  (testing "Lines were not properly parsed"
+    (is (= ["this, is", "a, test"] (parseLines "this, is\r\na, test")))))
+
+(comment Test if the function that glues parseCommas and parseLines works )
+(deftest parseCsvFromString-Test
+  (testing "Could not properly parse commas within each line"
+    (is (= [["this", "is"], ["a", "test"]] (parseCsvFromString "this,is\r\na,test")))))
+
+(comment Test for readFile, if we can read a file into something)
+(deftest readFile-Test
+  (testing "Could not read example file"
+      (is (= "this,is,a,test\r\nthis,is,line,two\r\nthree,should,be,last,line" (readFile "D:\\Projects\\Clojure\\Wednesday\\test\\testfile.txt")))))
+
+
+  (comment Test if sending a filename we can read the strings from it)
+  (deftest readFile-Test
+    (testing "The file wasnt parsed properly"
+      (is (= [["this", "is", "a", "test"], ["this", "is", "line", "two"], ["three", "should", "be", "last", "line"]] (parseFile "D:\\Projects\\Clojure\\Wednesday\\test\\testfile.txt")))))
+
+
+
+
+
